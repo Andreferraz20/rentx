a921033556fd2aba22e56b5e2e5d199b6f1b8b05
Andre Ferraz <luizlopesferraz22@gmail.com>
2021-03-18 17:44:11 +0000

    Principio de responsabilidade unica. Services

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,27 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+/**
+ * Definir o tipo de retorno X
+ * Alterar o retorno de erro X
+ * Acessar o repositório X
+ */
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  execute({ description, name }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+    if (categoryAlreadyExists) {
+      throw new Error("Category Already Exists");
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService };
