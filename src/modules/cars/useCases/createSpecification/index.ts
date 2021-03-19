cdf6ece8f87984f310f130d7564a16fc0710ae22
Andre Ferraz <luizlopesferraz22@gmail.com>
2021-03-19 18:06:37 +0000

    Creating specification controller and useCase

---
+++ src/modules/cars/useCases/createSpecification/index.ts
@@ -0,0 +1,15 @@
+import { SpecificationsRepository } from "../../repositories/implementations/SpecificationsRepository";
+import { CreateSpecificationController } from "./CreateSpecificationController";
+import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
+
+const specifcationsRepository = new SpecificationsRepository();
+
+const createSpecificationUseCase = new CreateSpecificationUseCase(
+  specifcationsRepository
+);
+
+const createSpecificationController = new CreateSpecificationController(
+  createSpecificationUseCase
+);
+
+export { createSpecificationController };
