f66d0f69783c194980ef1cd3c6d71dc3b62cff37
Andre Ferraz <luizlopesferraz22@gmail.com>
2021-03-24 12:08:23 +0000

    apartir daqui começa o erro

--- src/modules/cars/repositories/implementations/CategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -10,15 +10,8 @@ class CategoriesRepository implements ICategoriesRepository {
     private repository: Repository<Category>;
     private static INSTANCE: CategoriesRepository;
 
-    private constructor() {
+    constructor() {
         this.repository = getRepository(Category);
-    }
-
-    public static getInstance(): CategoriesRepository {
-        if (!CategoriesRepository.INSTANCE) {
-            CategoriesRepository.INSTANCE = new CategoriesRepository();
-        }
-        return CategoriesRepository.INSTANCE;
     }
 
     async create({ description, name }: ICreateCategoryDTO): Promise<void> {
