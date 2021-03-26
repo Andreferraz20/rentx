8f440de7cc7fca812efbd33f3169c9bb642866d6
Andre Ferraz <luizlopesferraz22@gmail.com>
2021-03-26 09:00:16 +0000

    migrations, tsyringe

--- src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -1,18 +1,23 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
 
 class CreateSpecificationController {
-    constructor(
-        private createSpecificationUseCase: CreateSpecificationUseCase
-    ) {}
+    async handle(request: Request, response: Response): Promise<Response> {
+        try {
+            const { name, description } = request.body;
 
-    handle(request: Request, response: Response): Response {
-        const { name, description } = request.body;
+            const createSpecificationUseCase = container.resolve(
+                CreateSpecificationUseCase
+            );
 
-        this.createSpecificationUseCase.execute({ name, description });
+            await createSpecificationUseCase.execute({ name, description });
 
-        return response.status(201).send();
+            return response.status(201).send();
+        } catch (err) {
+            return response.status(500).json({ error: err.message });
+        }
     }
 }
 
--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,3 +1,5 @@
+import { inject, injectable } from "tsyringe";
+
 import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
 
 interface IRequest {
@@ -5,17 +7,21 @@ interface IRequest {
     description: string;
 }
 
+@injectable()
 class CreateSpecificationUseCase {
-    constructor(private specificationsRepository: ISpecificationsRepository) {}
-    execute({ name, description }: IRequest): void {
-        const specificationAlreadyExists = this.specificationsRepository.findByName(
+    constructor(
+        @inject("SpecificationsRepository")
+        private specificationsRepository: ISpecificationsRepository
+    ) {}
+    async execute({ name, description }: IRequest): Promise<void> {
+        const specificationAlreadyExists = await this.specificationsRepository.findByName(
             name
         );
         if (specificationAlreadyExists) {
             throw new Error(`Specification ${name} already exists`);
         }
 
-        this.specificationsRepository.create({ name, description });
+        await this.specificationsRepository.create({ name, description });
     }
 }
 
