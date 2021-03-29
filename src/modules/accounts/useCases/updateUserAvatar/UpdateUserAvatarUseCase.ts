a0068e5abcb3892e05883abdb86081dbfdb636a0
Andre Ferraz <luizlopesferraz22@gmail.com>
2021-03-29 13:23:17 +0000

    Delete when add an avatar

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -6,6 +6,7 @@
 
 import { inject, injectable } from "tsyringe";
 
+import { deleteFile } from "../../../../utils/file";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -22,6 +23,10 @@ class UpdateUserAvatarUseCase {
 
     async execute({ user_id, avatar_file }: IRequest): Promise<void> {
         const user = await this.usersRepository.findById(user_id);
+
+        if (user.avatar) {
+            await deleteFile(`./tmp/avatar/${user.avatar}`);
+        }
 
         user.avatar = avatar_file;
 
---
+++ src/utils/file.ts
@@ -0,0 +1,10 @@
+import fs from "fs";
+
+export const deleteFile = async (filename: string) => {
+    try {
+        await fs.promises.stat(filename);
+    } catch (err) {
+        return;
+    }
+    await fs.promises.unlink(filename);
+};
