8a775fa1f6cb4bb096a9247498aff451a6b0f80c
Andre Ferraz <luizlopesferraz22@gmail.com>
2021-03-29 12:08:03 +0000

    upload de avatar

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,32 @@
+// Adicionar coluna avatar na tabela de users
+// Refatorar usuário com coluna avatar
+// Configuração upload multer
+// Criar regra de negócio do upload
+// Criar o controller
+
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+    user_id: string;
+    avatar_file: string;
+}
+
+@injectable()
+class UpdateUserAvatarUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+
+    async execute({ user_id, avatar_file }: IRequest): Promise<void> {
+        const user = await this.usersRepository.findById(user_id);
+
+        user.avatar = avatar_file;
+
+        await this.usersRepository.create(user);
+    }
+}
+
+export { UpdateUserAvatarUseCase };
