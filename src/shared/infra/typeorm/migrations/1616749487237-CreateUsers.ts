64057cfbce462a34428606b4e743206c47dae1f9
Andre Ferraz <luizlopesferraz22@gmail.com>
2021-05-16 16:39:05 +0000

    Adjust primary user

--- src/shared/infra/typeorm/migrations/1616749487237-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1616749487237-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1616749487237 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
